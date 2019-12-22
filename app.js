019dcede74d314b7bcaea68196f489df16bd51b8
GihanAyesh <gihanayeshmanthadrck@gmail.com>
2019-12-22 18:42:49 +0530

    adding expressjs and bodyparser

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-const http = require('http');
-const router = require('./routes.js');
+const express = require('express');
+const bodyParser = require ('body-parser');
 
-const server = http.createServer (router.handler);
+const app=express();
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add_product',(req,res,next)=>{
+    res.send("<link rel='icon' href='data:,'><form action='/get_product' method='POST'><input type='text' name='title'><button type='submit'>Add</button></form>");
+});
+
+app.use('/get_product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect("/");
+});
+
+app.use('/',(req,res,next)=>{
+    res.send("<h1>HI</h1>")
+});
+
+app.listen(3000);
